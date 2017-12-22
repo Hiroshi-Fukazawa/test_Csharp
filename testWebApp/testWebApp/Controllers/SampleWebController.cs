c55011a566ead49899f613410bca583dc31b2d31
Hiroshi-Fukazawa <fukazawa@fullness.co.jp>
2017-12-22 12:09:01 +0900

    Sampleソリューション追加

---
+++ testWebApp/testWebApp/Controllers/SampleWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testWebApp.Controllers
+{
+    public class SampleWebController : Controller
+    {
+        // GET: SampleWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
